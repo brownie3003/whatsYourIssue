00d378d6467af5b07c403d2c3dda0ae5fd129fa7
Andy Brown <brownie3003@gmail.com>
2014-07-15 03:28:38 +0100

    init commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,43 @@
+$(function() {
+    function loadshit(responseData) {
+        console.log("YOYOY");
+        $("#mpName").html(responseData.mpName);
+        $("#constituency").html(responseData.mpConstituency)
+        $("#mpPhoto").attr('src', responseData.image)
+        $(".email a").attr('href', responseData.email + "?subject=Data Retention and Investigatory Powers Bill&body= Dear " + responseData.mpName + ", %0D%0A %0D%0A I am writing becasue I am greatly concerned about the Data Retention and Investigatory Powers Bill that is being rushed through parliment by all parties. I think this shows gross disregard for the political process. I consider this a very serious issue and I think it should at least be discussed in parliment. %0D%0A %0D%0A I hope that you will consider the constituents that elected you before voting on this bill.")
+        createTweetLink(responseData.twitter)
+        $(".two").fadeOut(function() {
+            $(".three").fadeIn();
+        })
+    }
+
+    function createTweetLink(handle) {
+        var twtTitle  = "Hey " + handle + " I'm not happy that you're voting for DRIP without even debating it #voteAgainstDRIP";
+        var twtUrl    = location.href;
+        var maxLength = 140 - (twtUrl.length + 1);
+        if (twtTitle.length > maxLength) {
+        twtTitle = twtTitle.substr(0, (maxLength - 3))+'...';
+        }
+        var twtLink = 'http://twitter.com/home?status='+encodeURIComponent(twtTitle + ' ' + twtUrl);
+        $(".tweet a").attr('href', twtLink);
+    }
+
+    $(".yes").click(function() { 
+        $(".one").fadeOut(function() {
+            $(".two").fadeIn();
+        });
+    });
+
+    $(".postcode").submit(function(e) {
+        e.preventDefault();
+        var data = {postcode: $("input").val()}
+
+        $.ajax({
+            url: "http://localhost:3000/getMP",
+            data: data,
+            success: function (responseData) {
+                loadshit(responseData);
+            }
+        })
+    })
+})
---
+++ routes/getMP.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    var postcode = req.query.postcode;
+    var url =  "http://findyourmp.parliament.uk/api/search?q=" + postcode + "&f=js"
+    var data;
+
+    getMPdata(url, data, res)
+});
+
+function getMPdata(url, data, res) {
+    request(url, function(err, resp, body) {
+        var result = JSON.parse(body);
+
+        data = {
+            mpName : result.results.constituencies[0].member_name,
+            mpConstituency : result.results.constituencies[0].constituency_name,
+            mpBiography : result.results.constituencies[0].member_biography_url
+        }
+
+        scrapeBio(data, res);
+    });
+}
+
+function scrapeBio (data, res) {
+    request(data.mpBiography, function(err, resp, html) {
+        $ = cheerio.load(html);
+        data["email"] = $("#ctl00_ctl00_FormContent_SiteSpecificPlaceholder_PageContent_ctlContactDetails_rptPhysicalAddresses_ctl00_hypEmail").attr("href");
+        data["twitter"] = $("#ctl00_ctl00_FormContent_SiteSpecificPlaceholder_PageContent_ctlContactDetails_rptSocialMedia_ctl02_hypLink").text();
+        data["image"] = $("#ctl00_ctl00_FormContent_SiteSpecificPlaceholder_PageContent_ctlMemberImage_imgMember").attr("src");
+        console.log(data);
+
+        res.send(data)
+    });
+}
+
+module.exports = router;
